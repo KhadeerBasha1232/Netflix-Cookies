25aa4d9b7253aa5e0042d47ad4b747a848a3bcb1
KhadeerBasha1232 <120262074+KhadeerBasha1232@users.noreply.github.com>
2024-02-04 21:53:37 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('randomize-content/', views.get_random_content, name='randomize_content'),
     path('',views.index,name="Index"),
     path('upload/', views.post , name='upload'),
+    path('delete-cookies/', views.delete_cookies, name='delete_cookies'),
     # Add other URLs as needed
 ]
